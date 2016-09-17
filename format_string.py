9eb4b5ba57e63c1b731f104e213197a9e5f054b8
Mary Kuzmenko <mary.kuzmenko@gmail.com>
2016-09-17 13:07:31 +0300

    Add files via upload

---
+++ format_string.py
@@ -0,0 +1,13 @@
+my_name = "Mary"
+
+my_favourite_song = "We are the Champions"
+
+my_favourite_place = "Parents' home"
+
+my_favourite_fruits = "apples"
+
+my_age = 31
+
+print "Let's talk about %s, she is %d." %(my_name, my_age)
+print "Her favourite song is %s by Queen." %my_favourite_song
+print "She likes to be at %s with her parents and eat %s with them." %(my_favourite_place, my_favourite_fruits)
