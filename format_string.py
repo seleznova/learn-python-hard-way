8e9e6d70dcf062cf846777d7d94a3a412ca5ec4e
Mary Kuzmenko <mary.kuzmenko@gmail.com>
2016-09-17 14:06:31 +0300

    Add files via upload

--- format_string.py
+++ format_string.py
@@ -11,3 +11,17 @@ my_age = 31
 print "Let's talk about %s, she is %d." %(my_name, my_age)
 print "Her favourite song is %s by Queen." %my_favourite_song
 print "She likes to be at %s with her parents and eat %s with them." %(my_favourite_place, my_favourite_fruits)
+
+#Here is some new srange stuff :)
+days = "Mon Tue Wed Shu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days:", days
+print "Here are the months:", months
+
+print """
+    There's something going on here.
+    With the three double-quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if we want, or 5, or 6.
+    """
