fc584f8bf900e2f1242c55d08175fa557aaa9819
seleznova <mary.kuzmenko@gmail.com>
2016-09-19 16:24:51 +0300

    Add additional files with examples

---
+++ raw-input-and-argv.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variabe is:", second
+print "Your first variable is:", third
+
+print "How old are you?"
+
+age = raw_input()
+
+print "How tall are you?"
+
+height = raw_input()
